0f30d873797d4ccf7c41f4927929bd86a01a3f5a
tchartchke <chartai.kl@gmail.com>
2021-04-14 14:02:13 -0500

    commit tree 7a83423f21b834351a70ae87248c9098e2aceee1

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random()*10) + 1
+
+const num3 = 9
+const num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(1, 20)
